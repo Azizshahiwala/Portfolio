38704781d10e64ecc0415e0085f5b048eef4fe9e
Azizshahiwala <shahiwalaaziz8@gmail.com>
2026-03-29 11:48:26 +0530

    Base template ready for content integration now

---
+++ script.js
@@ -0,0 +1,5 @@
+
+//This adds an click event on class hamburger. Which is then use to open / close navigation bar using .toggle
+function toggleMenu(){
+    document.querySelector('nav').classList.toggle('open');
+}
